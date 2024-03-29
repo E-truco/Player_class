20be1de358d00806eb7c98a9baa13c7d1f4523ce
Cody Ko In A Box <codykoinabox@gmail.com>
2024-03-29 16:37:15 -0300

    Create Player.dart

---
+++ Player.dart
@@ -0,0 +1,10 @@
+class Player{
+  late int id;
+  late int points;
+  List<Card> hand = [];
+
+  Player(this.id, this.points, this.hand);
+
+  @override
+  String toString() => "ID: $id | Points: $points | Hand: $hand";
+}
